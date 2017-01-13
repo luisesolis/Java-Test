8bc086f9dcba31b7c722ea004c6ece5f6d195d29
luisesolis <luis.esolis483@gmail.com>
2017-01-13 12:49:58 -0600

    Mayus a Minus
    Nombre de la base de datos

--- src/com/banco/modelo/conexion/Conexion.java
+++ src/com/banco/modelo/conexion/Conexion.java
@@ -12,7 +12,7 @@ import java.sql.*;
  */
 public class Conexion {
     private static final String driver = "com.mysql.jdbc.Driver";
-    private static final String url = "jdbc:mysql://localhost:3306/bancoCB";
+    private static final String url = "jdbc:mysql://localhost:3306/bancocb";
     private static final String user = "cbts";
     private static final String password = "cbts";
     
